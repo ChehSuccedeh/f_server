d641239ae075f89343ecfea794fd26ff162c5cbf
Fabio <fabio.marmello@gmail.com>
2024-10-29 11:55:05 +0100

    added default constants.js

---
+++ constants.js
@@ -0,0 +1,19 @@
+/*
+    This file contains all the constants that are used in the project.
+    Please set this up in order to run the project.
+*/
+
+// Set the server URL here
+const SERVER_URL = 'http://localhost/';
+exports.SERVER_URL = SERVER_URL;
+// Set the server PORT here
+const SERVER_PORT = '3000';
+exports.SERVER_PORT = SERVER_PORT;
+
+// Set the first administrator username and password here: 
+// during the first run a privileged user with this credentials will be automatically created
+// feel free to remove this user after the first run
+const username = '';
+const password = '';
+exports.username = username;
+exports.password = password;
